3563cdd78d637360d5e3ff0609a270aecf9e5f72
smpchandru <smpchandru@gmail.com>
2018-12-13 11:14:54 +0530

    Add files via upload

---
+++ Main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("Hello Go"))
+}
+
+func main() {
+	fmt.Println("Webserver started")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
